2927ec33e16be77909b090106871823b15ca9aa4
NuskuEvan <132699270+nuskuevan@users.noreply.github.com>
2023-06-01 13:59:29 -0700

    Delete MEDIEVAL.png

--- app/src/main/java/com/example/wooloo2/MainActivity.java
+++ app/src/main/java/com/example/wooloo2/MainActivity.java
@@ -49,14 +49,55 @@ public class MainActivity extends AppCompatActivity {
 
                 String storyFantasy = "";
                 storyFantasy +="Welcome to Drenkon " + etName + " , be careful not to upset the natives of this place as you will face consequences";
-                storyFantasy += "You walk through the " + etNoun + " carefully not wanting to draw any attention to you";
-                storyFantasy += "";
+                storyFantasy += "You walk through The Forest of Dreams, be careful though because something " + etAdjective + " can show up.";
+                storyFantasy += "As you walk through the forest you spot a " + etAnimal + ", a very common creature in the forest.";
+                storyFantasy += "As you past The Forest of Dreams you come across a Wizard asking what your Alignment is " + etAlignment + " you said and asks you to follow him.";
+                storyFantasy += "You follow the Wizard " + etAdverb + ", he whispered something then turned around and looks at you.";
+                storyFantasy += "When he does that " + etNumber + " zombies show up surrounding you,he tells you that he is a Necromancer collecting travelers bodies to rule the world with his undead army.";
+                storyFantasy += "You feel " + etVerb + " about the situation, but steel your resolve and get ready for your first fight in this world.";
+                storyFantasy += "You draw your" + etWeapon + " and start using it to fight the zombies in front of you not knowing if you win or not.";
+                storyFantasy += "You start winning the battle and eventually slayed the last zombie, now looking at the Necromancer he looks " + etAdjective2 + ", but that doesn't stop you from killing him as well to stop him from making an army to rule the world.";
+                storyFantasy += "As you walk past his body you see the familiar place of " + etNoun + ", and start walking towards it.";
+                storyFantasy += "At last you make it only to find a note that says 'YOU HAVE " + etChoice + "' knowing that you finally remember everything.";
 
                 String storyApocalypse = "";
+                storyApocalypse += "";
+                storyApocalypse += "";
+                storyApocalypse += "";
+                storyApocalypse += "";
+                storyApocalypse += "";
+                storyApocalypse += "";
+                storyApocalypse += "";
+                storyApocalypse += "";
+                storyApocalypse += "";
+                storyApocalypse += "";
+                storyApocalypse += "";
 
                 String storyMedieval = "";
+                storyMedieval += "";
+                storyMedieval += "";
+                storyMedieval += "";
+                storyMedieval += "";
+                storyMedieval += "";
+                storyMedieval += "";
+                storyMedieval += "";
+                storyMedieval += "";
+                storyMedieval += "";
+                storyMedieval += "";
+                storyMedieval += "";
 
                 String storyHell = "";
+                storyHell += "";
+                storyHell += "";
+                storyHell += "";
+                storyHell += "";
+                storyHell += "";
+                storyHell += "";
+                storyHell += "";
+                storyHell += "";
+                storyHell += "";
+                storyHell += "";
+                storyHell += "";
 
                 WOOLOOGENERATION.putExtra("fantasy",storyFantasy);
                 WOOLOOGENERATION.putExtra("apocalypse",storyApocalypse);
